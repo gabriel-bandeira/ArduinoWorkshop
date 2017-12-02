9739fdf7b963397b08f2fc11ecc226c8bcf36984
George <gvictor00@gmail.com>
2017-12-02 00:02:53 -0200

    Adicionado o sistema de geração de sequência

--- 6_geniusBase/6_geniusBase.ino
+++ 6_geniusBase/6_geniusBase.ino
@@ -7,51 +7,98 @@
 */
 
 #define NUM_OF_LEDS 4
+#define MAXIMO 10
 
 // Setando os pinos digitais para serem os ledPins
-int ledPins[NUM_OF_LEDS] = {8, 9, 10, 11};
+int ledPins[NUM_OF_LEDS] = {10, 11, 12, 13};
 
 // Setando os pinos digitais para serem os buttonPins
-int buttonPins[NUM_OF_LEDS] = {4, 5, 6, 7};
+int buttonPins[NUM_OF_LEDS] = {2, 3, 4, 5};
 
+int freq[NUM_OF_LEDS] = {300, 500, 700, 900};
+int buzerPin = 6;
+
+int sequencia[MAXIMO] = {0,0,0,0,0,0,0,0,0,0};
+int i = 0;
+
+bool gerou = false;
 
 // Funcao setup, para inicializar alguns parametros
 void setup()
 {
-	// Setando os pinos dos leds como saida
-	for (int i = 0; i < NUM_OF_LEDS; i++)
-		pinMode(ledPins[i], OUTPUT);
-	
-	// Setando os pinos dos botoes como entrada
-	for (int i = 0; i < NUM_OF_LEDS; i++)
-		pinMode(buttonPins[i], INPUT);
+  // Setando os pinos dos leds como saida
+  for (int i = 0; i < NUM_OF_LEDS; i++)
+    pinMode(ledPins[i], OUTPUT);
 
-	// Liberando a comunicacao serial, para velocidade de 9600
-	Serial.begin(9600);
+  // Setando os pinos dos botoes como entrada
+  for (int i = 0; i < NUM_OF_LEDS; i++)
+    pinMode(buttonPins[i], INPUT_PULLUP);
+
+  pinMode(buzerPin, OUTPUT);
+
+  // Liberando a comunicacao serial, para velocidade de 9600
+  Serial.begin(9600);
+
+  // Dá a semente do random
+  randomSeed(analogRead(0));
 }
 
 // Funcao que fica rodando enquanto o arduino estiver ligado
 void loop()
 {
-	for (int i = 0; i < NUM_OF_LEDS; i++)
-	{
-		// salvando o valor da porta buttonPin, que esta o botao, na variavel buttonState
-		int buttonState = digitalRead(buttonPins[i]);
+  if(gerou == false)
+    geraOrdem(5);
+  
+  for (int i = 0; i < NUM_OF_LEDS; i++)
+  {
+    // salvando o valor da porta buttonPin, que esta o botao, na variavel buttonState
+    int buttonState = digitalRead(buttonPins[i]);
 
-		// imprime na tela serial o valor lido do botao
-		Serial.print("Estado do botao: ");
-		Serial.println(buttonState);
+    // imprime na tela serial o valor lido do botao
+    Serial.print("Estado do botao: ");
+    Serial.println(buttonState);
 
-		// Vendo se o botao esta pressionado para ativar o led
-		if(buttonState == HIGH)
-		{
-			// Acender o LED
-			digitalWrite(ledPins[i], HIGH);
-		} 
-		else
-		{
-			// Apagar o LED
-			digitalWrite(ledPins[i], LOW);
-		}
-	}
+    // Vendo se o botao esta pressionado para ativar o led
+    if (buttonState == LOW)
+    {
+      // Acender o LED
+      digitalWrite(ledPins[i], HIGH);
+
+      // Toca o buzzer
+      tone(buzerPin, freq[i]);
+    }
+    else
+    {
+      // Apagar o LED
+      digitalWrite(ledPins[i], LOW);
+
+      // Silencia o buzzer
+      noTone(buzerPin);
+    }
+  }
+}
+
+void geraOrdem(int qtd){
+  for (i = 0; i < qtd; i++){
+    sequencia[i] = random(1,5);
+    Serial.println(sequencia[i]);
+  }
+
+  i = 0;
+  while(sequencia[i] > 0){
+    
+      // Acender o LED
+      digitalWrite(ledPins[sequencia[i]-1], HIGH);
+      // Toca o buzzer
+      tone(buzerPin, freq[sequencia[i]-1]);
+      delay(400);
+
+      // Apagar o LED
+      digitalWrite(ledPins[sequencia[i]-1], LOW);
+      // Silencia o buzzer
+      noTone(buzerPin);
+      i++;
+  }
+
+  gerou = true;
 }
